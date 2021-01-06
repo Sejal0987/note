f841870f9ea43d626088fe5fc2e540f1fc57db23
Sejal Jaiswal <67636026+Sejal0987@users.noreply.github.com>
2021-01-06 23:58:01 +0530

    Update notes_screen.dart

--- lib/notes_screen.dart
+++ lib/notes_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'adding.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:intl/intl.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'edit_note.dart';
+
+User user;
 
 class NotesScreen extends StatefulWidget {
   static const String id = 'notes_screen';
@@ -12,6 +15,24 @@ class NotesScreen extends StatefulWidget {
 final _fireStore = FirebaseFirestore.instance;
 
 class _NotesScreenState extends State<NotesScreen> {
+  final _auth = FirebaseAuth.instance;
+  @override
+  void initState() {
+    super.initState();
+    getCurrentUser();
+  }
+
+  void getCurrentUser() async {
+    try {
+      user = _auth.currentUser;
+      if (user != null) {
+        print(user.email);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,32 +71,40 @@ class NoteStream extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
-      stream: _fireStore.collection('notes').orderBy('createdAt').snapshots(),
+      stream: _fireStore
+          .collection('notes')
+          .doc(user.uid)
+          .collection('mynotes')
+          .orderBy('createdAt')
+          .snapshots(),
       builder: (context, snapshot) {
-        List<NoteBubble> messageBubble = [];
+        List<NoteBubble> noteBubble = [];
         if (!snapshot.hasData) {
           return CircularProgressIndicator(
             backgroundColor: Colors.blueAccent,
           );
         }
         final message = snapshot.data.docs.reversed;
+        int i = 1;
         for (var msg in message) {
           final String msgText = msg.data()['note'];
           final msgSender = msg.data()['sender'];
           final title = msg.data()['title'];
           final date = msg.data()['date'];
           final currentUser = msgSender;
-          messageBubble.add(NoteBubble(
+          noteBubble.add(NoteBubble(
               sender: msgSender,
               text: msgText,
+              documents: msg,
               // isMe: currentUser == user.email,
               title: title,
               date: date));
+          i++;
         }
 
         return ListView(
           padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
-          children: messageBubble,
+          children: noteBubble,
         );
       },
     );
@@ -85,10 +114,10 @@ class NoteStream extends StatelessWidget {
 class NoteBubble extends StatelessWidget {
   final String text;
   final String sender;
-  final bool isMe;
+  final DocumentSnapshot documents;
   final String title;
   final date;
-  NoteBubble({this.sender, this.text, this.isMe, this.title, this.date});
+  NoteBubble({this.sender, this.text, this.documents, this.title, this.date});
   //String formatter = DateFormat('yMd').format(date);
 
   // DateTime dateToday =
@@ -96,59 +125,85 @@ class NoteBubble extends StatelessWidget {
   // 2016-01-25
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
-      child: Material(
-        borderRadius: BorderRadius.circular(30.0),
-        color: Colors.black38,
-        child: Column(
-          children: [
-            Text(
-              'Title:',
-              style: TextStyle(
-                fontWeight: FontWeight.w400,
-                fontSize: 30.0,
-                color: Colors.black,
-              ),
-              textAlign: TextAlign.start,
-            ),
-            SizedBox(
-              height: 5.0,
-            ),
-            Text('$title'),
-            Container(
-              padding: EdgeInsets.all(30.0),
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black), color: Colors.white),
-              margin: EdgeInsets.all(20.0),
-              child: Text(
-                '$text',
-                style: TextStyle(
-                  color: Colors.black,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return EditNote(
+            documentSnapshot: documents,
+          );
+        }));
+      },
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+        child: Material(
+          borderRadius: BorderRadius.circular(30.0),
+          color: Colors.black87,
+          child: Padding(
+            padding: EdgeInsets.only(top: 30.0),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.baseline,
+                  textBaseline: TextBaseline.alphabetic,
+                  children: [
+                    Text(
+                      'Title:',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 20.0,
+                        color: Colors.white,
+                      ),
+                      textAlign: TextAlign.start,
+                    ),
+                    SizedBox(
+                      width: 8.0,
+                    ),
+                    Text(
+                      '$title',
+                      style: TextStyle(
+                        color: Colors.white70,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(20.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Created:',
+                Container(
+                  padding: EdgeInsets.all(30.0),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black),
+                      color: Colors.white),
+                  margin: EdgeInsets.all(20.0),
+                  child: Text(
+                    '$text',
                     style: TextStyle(
-                      color: Colors.black38,
+                      color: Colors.black,
                     ),
                   ),
-                  Text(
-                    'Edited At:',
-                    style: TextStyle(
-                      color: Colors.black38,
-                    ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Created:',
+                        style: TextStyle(
+                          color: Colors.black38,
+                        ),
+                      ),
+                      Text(
+                        'Edited At:',
+                        style: TextStyle(
+                          color: Colors.black38,
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
